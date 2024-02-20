faae19f833490dc289d09c8d253859289cbc819c
Marcelo Meirelles <meirellesm@hotmail.com>
2024-02-19 21:36:00 -0300

    teste

--- band_manager/Program.cs
+++ band_manager/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound from Alura
+
+string welcomeMessage = "Boas vindas ao Screen Sound";
+Console.WriteLine(welcomeMessage);
